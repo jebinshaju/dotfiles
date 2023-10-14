341b8c01c9c02741bc6193866c186b1b376bcab1
Jebin Shaju <67820811+jebinshaju@users.noreply.github.com>
2023-10-14 11:32:27 +0530

    Add files via upload

---
+++ i3/set_wallpaper.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Directory containing your wallpaper images
+wallpaper_dir="/home/jebinshaju/Pictures/acar"
+
+# Set the initial wallpaper
+feh --bg-fill --randomize "$wallpaper_dir"/*
+
+# Loop indefinitely
+while true; do
+    # Sleep for 5 minutes
+    sleep 300
+
+    # Set a new random wallpaper
+    feh --bg-fill --randomize "$wallpaper_dir"/*
+done
+
